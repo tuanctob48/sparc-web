3c65587b718e38f67fac629c0e7e9dbf28d139b0
tuanctob48 <tuanctob48@gmail.com>
2017-07-14 10:06:59 +0700

    fix layout

--- resources/views/layouts/layout_member.blade.php
+++ resources/views/layouts/layout_member.blade.php
@@ -19,81 +19,90 @@ Change History  :
 </head>
 <header>
     <div class="container">
-         <div class="header_top row">
-           <div class="col-7 logo">
-             <h2>Welcome to SPARC LAB's' Website</h2>
-           </div>
-         </div>
-
-
-         <div class="meNu">
+        <div class="header_top row">
+          <div class="col-7 logo">
+            <h2>Welcome to SPARC LAB's' Website</h2>
+          </div>
+        </div>
+        <div class="meNu">
             <ul>
-              <li><a href="{{url('/home')}}">Trang chủ</a></li>
-              <li>
-                <a href="{{url('/members')}}">thông tin</a>
-                <ul id="submenu">
-                  <li><a href="{{url('/project')}}">Các dự án đang thực hiện</a></li>
-                  <li><a href="{{url('/members')}}">Thành viên lab</a></li>
-                  <li><a href="{{url('/awards')}}">Giải thưởng của lab</a></li>
-                  <li><a href="{{url('/recruit')}}">Tuyển thành viên</a></li>
-                </ul>
-              </li>           
-                  <?php 
-                    if(!isset($_SESSION))
-                    {
-                        session_start();
-                    }
-                    $value_login=0;
-                    // check user authen, if user has been authen then redirect to page home
-                    if(isset($_SESSION['status_authen']))
-                    {
-                        if($_SESSION['status_authen']==1)
-                        {
-                          $value_login=1;
-                        
-                          
-                        }
-                       // else
-                        //  echo @'<ul><li> <a href="{{url('/login')}}"> Đăng nhập </a></li></ul>'
-                    }
-                    if(1){
-                    ?>
-                      <li>
-                        <a href="{{url('/books')}}">Học tập</a>
-                        <ul id="submenu">
-                          <li><a href="{{url('/dienTu')}}">Kỹ thuật điện tử</a></li>
-                          <li><a href="{{url('/co')}}">Kỹ thuật cơ</a></li>
-                          <li><a href="{{url('/codding')}}">Kỹ thuật lập trình</a></li>
-                          <li><a href="{{url('/kyNangMem')}}">Kỹ năng mềm</a></li>
-                          <li><a href="{{url('/AI')}}">Trí tuệ nhận tạo</a></li>
-                          <li><a href="{{url('/books')}}">Sách tham khảo</a></li>
-                        </ul>
-                      </li>
-                      <li>
-                        <a href="{{url('/generalCode')}}">Quy định</a>
-                        <ul id="submenu">
-                          <li><a href="{{url('/generalCode')}}">Quy định chung của lab</a></li>
-                          <li><a href="{{url('/doProject')}}">làm dự án</a></li>
-                        </ul>
-                      </li>
-                      <li><a href="{{url('/news')}}">Tin tức</a></li>
-                      <li> <a href="{{url('/logout')}}">Đăng xuất </a></li>
-                    <?php
-                    }
-                    else
-                    {
-                      ?>
-                       <li> <a href="{{url('/login')}}"> Đăng nhập </a></li>
-                      <?php
-                    }
-                  ?>
-
+                <li><a href="{{url('/home')}}">Trang chủ</a></li>
+                <li>
+                    <a href="{{url('/members')}}">thông tin</a>
+                    <ul id="submenu">
+                        <li><a href="{{url('/project')}}">Các dự án đang thực hiện</a></li>
+                        <li><a href="{{url('/members')}}">Thành viên lab</a></li>
+                        <li><a href="{{url('/awards')}}">Giải thưởng của lab</a></li>
+                        <li><a href="{{url('/recruit')}}">Tuyển thành viên</a></li>
+                    </ul>
+                </li>           
+                <li>
+                    <a href="{{url('/books')}}">Học tập</a>
+                    <ul id="submenu">
+                        <li><a href="{{url('/dienTu')}}">Kỹ thuật điện tử</a></li>
+                        <li><a href="{{url('/co')}}">Kỹ thuật cơ</a></li>
+                        <li><a href="{{url('/codding')}}">Kỹ thuật lập trình</a></li>
+                        <li><a href="{{url('/kyNangMem')}}">Kỹ năng mềm</a></li>
+                        <li><a href="{{url('/AI')}}">Trí tuệ nhận tạo</a></li>
+                        <li><a href="{{url('/books')}}">Sách tham khảo</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="{{url('/generalCode')}}">Quy định</a>
+                    <ul id="submenu">
+                      <li><a href="{{url('/generalCode')}}">Quy định chung của lab</a></li>
+                      <li><a href="{{url('/doProject')}}">làm dự án</a></li>
+                    </ul>
+                </li>
+                <li><a href="{{url('/news')}}">Tin tức</a></li>
+                @php 
+                    if(!isset($_SESSION)) 
+                    { 
+                        session_start(); 
+                    } 
+                    $value_login=0; 
+                    $value_name="undefine"; // check user authen, if user has been authen then redirect to page home 
+                    
+                    if(isset($_SESSION['status_authen'])) { 
+                        if($_SESSION['status_authen']==AUTHEN_SUCCESS)
+                        { 
+                            $value_login=1; 
+                        } 
+                    } 
+                    if(isset($_SESSION['status_name'])) {
+                        $value_name = $_SESSION['status_name']; 
+                    } 
+                    if($value_login==1){
+                    @endphp
+                    <ul>
+                        <li> <a href="{{url('/logout')}}">Chao {{$value_name}} </a></li>
+                    </ul>
+                    @php if($_SESSION['status_admin'] == LEADER_STATUS || $_SESSION['status_admin'] == ADMIN_STATUS){ 
+                    @endphp
+                    <ul>
+                        <li><a href="{{url('/importdata')}}">Import Data</a></li>
+                    </ul>
+                    <ul>
+                        <li><a href="{{url('/importfile')}}">Import File</a></li>
+                    </ul>
+                    @php 
+                    } 
+                    @endphp 
+                    @php 
+                    }else{ 
+                    @endphp
+                    <ul>
+                        <li> <a href="{{url('/login')}}"> Đăng nhập </a></li>
+                    </ul>
+                    @php 
+                    } 
+                    @endphp
             </ul>
-         </div>
+        </div>
    </div>
 </header>
 
- <body>
+<body>
 
 @yield('content')
 
